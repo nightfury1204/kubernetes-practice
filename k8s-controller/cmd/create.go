a6a6318a9649bc0c85dd76da23d996ade2abfe56
nightfury1204 <nahid@appscode.com>
2017-12-15 17:17:19 +0600

    initial k8s controller

---
+++ k8s-controller/cmd/create.go
@@ -0,0 +1,184 @@
+// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"log"
+
+	"fmt"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	"github.com/golang/glog"
+
+	apiv1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/workqueue"
+)
+
+var Kubeconfig string
+
+type Controller struct {
+	indexer  cache.Indexer
+	queue    workqueue.RateLimitingInterface
+	informer cache.Controller
+}
+
+func NewController(queue workqueue.RateLimitingInterface, indexer cache.Indexer, informer cache.Controller) *Controller {
+	return &Controller{
+		informer: informer,
+		indexer:  indexer,
+		queue:    queue,
+	}
+}
+
+func (c *Controller) processNextItem() bool {
+
+	key, quit := c.queue.Get()
+	if quit {
+		return false
+	}
+
+	defer c.queue.Done(key)
+
+	err := c.syncToStdout(key.(string))
+
+	c.handleErr(err, key)
+	return true
+}
+
+func (c *Controller) syncToStdout(key string) error {
+	obj, exists, err := c.indexer.GetByKey(key)
+	if err != nil {
+		glog.Errorf("Fetching object with key %s from store failed with %v", key, err)
+		return err
+	}
+
+	if !exists {
+		fmt.Printf("Pod %s does not exist anymore\n", key)
+	} else {
+		fmt.Printf("Sync/Add/Update for Pod %s\n", obj.(*apiv1.Pod).GetName())
+	}
+	return nil
+}
+
+// handleErr checks if an error happened and makes sure we will retry later.
+func (c *Controller) handleErr(err error, key interface{}) {
+	if err == nil {
+		c.queue.Forget(key)
+		return
+	}
+
+	if c.queue.NumRequeues(key) < 5 {
+		glog.Infof("Error syncing pod %v: %v", key, err)
+		c.queue.AddRateLimited(key)
+		return
+	}
+
+	c.queue.Forget(key)
+	runtime.HandleError(err)
+	glog.Infof("Dropping pod %q out of the queue: %v", key, err)
+}
+
+func (c *Controller) Run(threadiness int, stopCh chan struct{}) {
+	defer runtime.HandleCrash()
+
+	defer c.queue.ShutDown()
+	glog.Info("Starting Pod controller")
+
+	go c.informer.Run(stopCh)
+
+	if !cache.WaitForCacheSync(stopCh, c.informer.HasSynced) {
+		runtime.HandleError(fmt.Errorf("Timed out waiting for caches to sync"))
+		return
+	}
+
+	go wait.Until(c.runWorker, time.Second, stopCh)
+
+	<-stopCh
+
+	glog.Info("Stopping Pod controller")
+}
+
+func (c *Controller) runWorker() {
+	for c.processNextItem() {
+	}
+}
+
+// createCmd represents the create command
+var CreateCmd = &cobra.Command{
+	Use:   "create",
+	Short: "create controller",
+	Run: func(cmd *cobra.Command, args []string) {
+		config, err := clientcmd.BuildConfigFromFlags("", Kubeconfig)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		podListWatcher := cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(), "pods", apiv1.NamespaceDefault, fields.Everything())
+
+		queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+		indexer, informer := cache.NewIndexerInformer(podListWatcher, &apiv1.Pod{}, 0, cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				key, err := cache.MetaNamespaceKeyFunc(obj)
+				if err == nil {
+					queue.Add(key)
+				}
+			},
+			UpdateFunc: func(old interface{}, new interface{}) {
+				key, err := cache.MetaNamespaceKeyFunc(new)
+				if err == nil {
+					queue.Add(key)
+				}
+			},
+			DeleteFunc: func(obj interface{}) {
+				key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+				if err == nil {
+					queue.Add(key)
+				}
+			},
+		}, cache.Indexers{})
+
+		controller := NewController(queue, indexer, informer)
+
+		indexer.Add(&apiv1.Pod{
+			ObjectMeta: metaV1.ObjectMeta{
+				Name:      "mypod",
+				Namespace: apiv1.NamespaceDefault,
+			},
+		})
+
+		// Now let's start the controller
+		stop := make(chan struct{})
+		defer close(stop)
+		go controller.Run(1, stop)
+
+		// Wait forever
+		select {}
+	},
+}
---
+++ k8s-controller/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"kubernetes-practice/k8s-controller/cmd"
+
+	"github.com/spf13/cobra"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+	home := homedir.HomeDir()
+
+	rootCmd := &cobra.Command{
+		Use: "controller",
+		Short:"Create k8s controller",
+	}
+
+	rootCmd.PersistentFlags().StringVarP(&cmd.Kubeconfig, "configPath", "c", home+"/.kube/config", "kube config path")
+	rootCmd.AddCommand(cmd.CreateCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
