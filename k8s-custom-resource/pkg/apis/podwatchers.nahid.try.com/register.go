2fe5b949085d16a2bd7da73f54266978c1f3f5a6
nightfury1204 <nahid@appscode.com>
2018-01-25 19:49:17 +0600

    trash

--- k8s-custom-resource/pkg/apis/podwatchers.nahid.try.com/register.go
+++
@@ -1,5 +0,0 @@
-package podwatchers_nahid_try_com
-
-const (
-	GroupName = "podwatchers.nahid.try.com"
-)
--- k8s-custom-resource/pkg/apis/podwatchers.nahid.try.com/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// +k8s:deepcopy-gen=package
-
-// +groupName=podwatchers.nahid.try.com
-package v1alpha1
